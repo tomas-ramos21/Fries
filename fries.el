234ed500c1c3a3749fec1d6fcd88ec9c6564c169
Tomas Ramos <tomas.ramos@apple.com>
2021-02-25 13:42:37 +0800

    Added initial seed file. Starting version branches

---
+++ fries.el
@@ -0,0 +1,36 @@
+;;; fries.el --- Disassemble Java byte-code for the current file
+;;
+;; Copyright (C) 2021 Tomas Aleixo Ramos
+;;
+;; Author: Tomas Ramos <https://github.com/tomas-ramos21>
+;; Maintainer: Tomas Ramos <tomas.ramosv21@gmail.com>
+;; Created: February 25, 2021
+;; Modified: February 25, 2021
+;; Version: 0.0.1
+;; Keywords: tools
+;; Homepage: https://github.com/tomas-ramos21/Fries
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+;;
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(provide 'fries)
+;;; fries.el ends here
